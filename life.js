07fb6e1fc0c25c674bd4c5cacba598fa05e6fc56
espressomint <espressomint06@gmail.com>
2024-10-07 12:03:42 +0200

    add switch logic for dealer and player hp, fix typos, make health go between 2 and 3,

--- life.js
+++ life.js
@@ -1,14 +1,12 @@
-// let global_lives = Math.floor(Math.random() * 6) + 1
-let player = 1
-// let dealer = parseInt(global_lives)
+let global_lives = Math.floor(Math.random() * 2) + 2
+let player = parseInt(global_lives)
+let dealer = parseInt(global_lives)
 let bullets = []
 let live_bullets = 0
 let blank_bullets = 0
 for(let i = 0; i < 6; i++){
-    bullets.push(Math.floor(Math.random(1) * 2))
-    
+    bullets.push(Math.floor(Math.random() * 2))
 }
-
 for (let n = 0; n < bullets.length; n++){
     if(bullets[n] == 1){
         live_bullets++
@@ -16,29 +14,83 @@ for (let n = 0; n < bullets.length; n++){
         blank_bullets++
     }
 }
+//zero bullets of each type check
+// if(live_bullets || blank_bullets == 0){
+//     window.location.reload()
+// }
 
+console.log(bullets)
 function shootYourself(){
     current_bullet = bullets.shift()
     if(current_bullet == 1 ){
         player--
+        alert("you shot yourself")
+        let life_display = document.getElementById("display_life");
+        life_display.innerHTML = ("player: " + player)
+        
+        let dealer_display = document.getElementById("display_dealer_life");
+        dealer_display.innerHTML = ("dealer: " + dealer)
+    }else{
+        dealer--
+        alert("you were lucky")
+        let life_display = document.getElementById("display_life");
+        life_display.innerHTML = ("player: " + player)
+        
+        let dealer_display = document.getElementById("display_dealer_life");
+        dealer_display.innerHTML = ("dealer: " + dealer)
     }
-    let life_display = document.getElementById("display_life");
-    life_display.innerHTML = ("player: " + player)
-    console.log(player)
-    console.log(current_bullet)
-    if(player == 0){
-        alert("dead")
-        window.location.reload()
-    }
-    if(bullets.length == 0){
-        alert("you win!")
-        window.location.reload()
+    switch (true){
+        case dealer == 0:
+            alert("he dead");
+            window.location.reload()
+            break;
+        case player == 0:
+            alert("you dead")
+            window.location.reload()
+            break;
+        case bullets.length == 0:
+            alert("you both live")
+            window.location.reload()
+            break;
     }
 }
+function shootThem(){
+    current_bullet = bullets.shift()
+    if(current_bullet == 1){
+        dealer--
+        alert("you scored")
+        let life_display = document.getElementById("display_life");
+        life_display.innerHTML = ("player: " + player)      
+        let dealer_display = document.getElementById("display_dealer_life");
+        dealer_display.innerHTML = ("dealer: " + dealer)
+    }else{
+        player--
+        alert("missed your shot")
+        let life_display = document.getElementById("display_player_life");
+        life_display.innerHTML = ("player: " + player)      
+        let dealer_display = document.getElementById("display_dealer_life");
+        dealer_display.innerHTML = ("dealer: " + dealer)
+    }
+    switch (true){
+        case dealer == 0:
+            alert("he dead");
+            window.location.reload()
+            break;
+        case player == 0:
+            alert("you dead")
+            window.location.reload()
+            break;
+        case bullets.length == 0:
+            alert("you both live");
+            window.location.reload()
+    }
+}
+//display shit
 let blank_display = document.getElementById("display_blanks");
 blank_display.innerHTML = ("blank bullets: " +blank_bullets)
 let live_display = document.getElementById("display_lives");
 live_display.innerHTML = ("live bullets: " + live_bullets)
-// let dealer_life_display = document.getElementById("display_dealer_life");
-// dealer_life_display.innerHTML = ("dealer: " + dealer)
-
+let life_display = document.getElementById("display_player_life");
+life_display.innerHTML = ("player: " + player)
+let dealer_display = document.getElementById("display_dealer_life");
+dealer_display.innerHTML = ("dealer: " + dealer)
