47c55e399ffd360d32ac2ff7b3c035769cf085ed
espressomint <espressomint06@gmail.com>
2024-10-07 12:58:11 +0200

    add rules, more foramtting, css style, add money, tweak logic

--- life.js
+++ life.js
@@ -4,6 +4,7 @@ let dealer = parseInt(global_lives)
 let bullets = []
 let live_bullets = 0
 let blank_bullets = 0
+let money = 0
 for(let i = 0; i < 6; i++){
     bullets.push(Math.floor(Math.random() * 2))
 }
@@ -24,66 +25,67 @@ function shootYourself(){
     current_bullet = bullets.shift()
     if(current_bullet == 1 ){
         player--
+        money = money - 10
         alert("you shot yourself")
-        let life_display = document.getElementById("display_life");
-        life_display.innerHTML = ("player: " + player)
-        
-        let dealer_display = document.getElementById("display_dealer_life");
-        dealer_display.innerHTML = ("dealer: " + dealer)
     }else{
         dealer--
+        money = money + 30
         alert("you were lucky")
-        let life_display = document.getElementById("display_life");
-        life_display.innerHTML = ("player: " + player)
-        
-        let dealer_display = document.getElementById("display_dealer_life");
-        dealer_display.innerHTML = ("dealer: " + dealer)
     }
     switch (true){
         case dealer == 0:
-            alert("he dead");
+            money = money + 100
+            alert("he dead, you earned: " + money + "$");
             window.location.reload()
             break;
         case player == 0:
-            alert("you dead")
+            money = money = 0
+            alert("you dead, you earned: nothing lmao" );
             window.location.reload()
             break;
         case bullets.length == 0:
+            money = money + 15
             alert("you both live")
             window.location.reload()
             break;
     }
+    let life_display = document.getElementById("display_player_life");
+    life_display.innerHTML = ("player: " + player)
+    let dealer_display = document.getElementById("display_dealer_life");
+    dealer_display.innerHTML = ("dealer: " + dealer)
 }
 function shootThem(){
     current_bullet = bullets.shift()
     if(current_bullet == 1){
+        money = money + 50
         dealer--
         alert("you scored")
-        let life_display = document.getElementById("display_life");
-        life_display.innerHTML = ("player: " + player)      
-        let dealer_display = document.getElementById("display_dealer_life");
-        dealer_display.innerHTML = ("dealer: " + dealer)
     }else{
+        money = money - 15
         player--
         alert("missed your shot")
-        let life_display = document.getElementById("display_player_life");
-        life_display.innerHTML = ("player: " + player)      
-        let dealer_display = document.getElementById("display_dealer_life");
-        dealer_display.innerHTML = ("dealer: " + dealer)
     }
     switch (true){
         case dealer == 0:
-            alert("he dead");
+            money =  money + 100
+            alert("he dead, you earned: " + money + "$");
             window.location.reload()
             break;
         case player == 0:
+            money = money = 0
             alert("you dead")
             window.location.reload()
             break;
         case bullets.length == 0:
-            alert("you both live");
+            money = money + 15
+            alert("you both live")
             window.location.reload()
+            break;
     }
+    let life_display = document.getElementById("display_player_life");
+    life_display.innerHTML = ("player: " + player)
+    let dealer_display = document.getElementById("display_dealer_life");
+    dealer_display.innerHTML = ("dealer: " + dealer)
 }
 //display shit
 let blank_display = document.getElementById("display_blanks");
